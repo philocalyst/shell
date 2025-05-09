8e1fe14ba43bd10c377bfdcd1489eca91ff45058
Miles Wirht <regime_skylark.0y@icloud.com>
2025-05-09 13:22:02 -0400

    💥ADDED💥 Basic argument capturing
    And split by delimteter? Following a C guide and am getting the
    impression that this might not be necessary for a rust implementation
    using the std??

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,22 @@ fn main() {
     if std::io::stdout().is_terminal() {
         // Display a prompt for the user.
         print!("{:?}", current_dir());
+
+        // Get command line arguments
+        let mut args: Args = std::env::args();
+
+        // Skip the program name
+        args.next();
+
+        let delimeter = ',';
+        let mut tokens: Vec<String> = Vec::new();
+        while let Some(arg) = args.next() {
+            for piece in arg.split(delimeter) {
+                tokens.push(piece.to_string());
+            }
+        }
+
+        print!("{:?}", tokens);
     } else {
         // No need to show prompt, not interactive.
     }
