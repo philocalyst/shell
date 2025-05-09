4080193df2ea5360f3c913a1548c918e69d26877
unknown <regime_skylark.0y@icloud.com>
2025-05-09 17:11:29 -0400

    💫CHANGED💫 Now reading from stdin!!!
    It's happening!!

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
     error::Error,
     fmt::{Arguments, Result},
     fs,
-    io::IsTerminal,
+    io::{BufRead, IsTerminal},
     os::unix::process,
     path::{Path, PathBuf},
     process::{Command, ExitCode, ExitStatus},
@@ -14,31 +14,44 @@ use std::io;
 
 use is_terminal;
 fn main() -> std::process::ExitCode {
+    // For now loading the PATH var once, before starting command capturing, because I need to think on what is an actual way to do this performantly. That is, the loading of new paths...
+    let path_var = env::var("PATH").unwrap();
+    let paths: Vec<PathBuf> = env::split_paths(&path_var).collect();
+    let map = map_executables(paths).unwrap();
+
     if std::io::stdout().is_terminal() {
-        // Display a prompt for the user.
+        // Display a prompt for the user :)
         print!("{:?}", current_dir());
 
-        // Get command line arguments
-        let mut args: Args = std::env::args();
+        loop {
+            let stdin = &io::stdin();
 
-        // Skip the program name
-        args.next();
-
-        let delimeter = ',';
-        let mut tokens: Vec<String> = Vec::new();
-        while let Some(arg) = args.next() {
-            for piece in arg.split(delimeter) {
-                tokens.push(piece.to_string());
+            // Get command line arguments
+            let mut args = Vec::new();
+            for line in stdin.lock().lines() {
+                println!("{:?}", &line);
+                args.push(line.unwrap());
             }
-        }
 
-        let path_var = env::var("PATH").unwrap();
-        let paths: Vec<PathBuf> = env::split_paths(&path_var).collect();
-        let map = map_executables(paths).unwrap();
+            // Skip the program name
 
-        if let Some(process) = map.get(&tokens[0]) {
-            let output = Command::new(process).output().unwrap();
-            println!("{:?}", output);
+            let delimeter = "&&";
+            let mut tokens: Vec<String> = Vec::new();
+            while let Some(arg) = args.clone().into_iter().next() {
+                for piece in arg.split(delimeter) {
+                    tokens.push(piece.to_string());
+                }
+            }
+            println!("{:?}", args);
+
+            if let Some(process) = map.get(&tokens[0]) {
+                let output = Command::new(process).output().unwrap();
+                println!("{:?}", output);
+            } else {
+                // If the process is not found..
+                println!("rash: Unknown command: {}", tokens[0])
+            }
+            break;
         }
 
         return ExitCode::SUCCESS;
