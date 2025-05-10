1ab3ff4d7219f6d7aba0a366156ea7a7d0b4dd01
unknown <regime_skylark.0y@icloud.com>
2025-05-10 00:26:21 -0400

    💫CHANGED💫 Moved prompt display out of launch_command
    For the chaining case again, you don't want the prompt to show up on the second iteration...

--- src/main.rs
+++ src/main.rs
@@ -103,34 +103,12 @@ fn main() -> std::process::ExitCode {
             for command in command_store {
                 launch_command(&command, &map);
             }
+            display_prompt(); // Then show prompt
 
-            // Try to parse the argument into a builtin. If the operation fails, we can assume that it's not a supported builtin, and can be tested agaisnt the PATH.
-            match command.as_str().parse::<Builtin>() {
-                Ok(command) => {
-                    match command {
-                        Builtin::Exit => break 'main,
-                        Builtin::Export => export::export(&tokens),
-                        Builtin::CD => change_directory::cd(&PathBuf::from(options[0].clone())),
-                    }
-                    display_prompt(); // Then show prompt
-                }
-                Err(_) => {
-                    // Command seems to be external, try and find it and execute it.
-                    if let Some(process) = map.get(command) {
-                        let code = run_command(&process, options);
-                        // Display error
-                        display_prompt(); // Then show prompt
-                    } else {
-                        // If the process is not found..
-                        println!("rash: Unknown command: {}", tokens[0]);
-                        display_prompt(); // Then show prompt
-                    }
-                }
             if 2 == 1 {
                 break;
             }
         }
-
         return ExitCode::SUCCESS;
     } else {
         // No need to show prompt, not interactive.
@@ -149,23 +127,17 @@ fn launch_command(
 
     // Try to parse the argument into a builtin. If the operation fails, we can assume that it's not a supported builtin, and can be tested agaisnt the PATH.
     match command.as_str().parse::<Builtin>() {
-        Ok(command) => {
-            match command {
-                Builtin::Exit => std::process::exit(0),
-                Builtin::Export => export::export(&argument_components),
-                Builtin::CD => change_directory::cd(&PathBuf::from(options[0].clone())),
-            }
-            display_prompt(); // Then show prompt
-        }
+        Ok(command) => match command {
+            Builtin::Exit => std::process::exit(0),
+            Builtin::Export => export::export(&argument_components),
+            Builtin::CD => change_directory::cd(&PathBuf::from(options[0].clone())),
+        },
         Err(_) => {
             // Command seems to be external, try and find and execute it.
             if let Some(command) = available_commands.get(command) {
                 let code = run_command(&command, options);
-                // Display error
-                display_prompt(); // Then show prompt
             } else {
                 // If the process is not found..
-                println!("rash: Unknown command: {}", argument_components[0]);
                 display_prompt(); // Then show prompt
             }
         }
